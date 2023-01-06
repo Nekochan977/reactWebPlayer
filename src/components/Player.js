6263bf69a0b7c217142fc203fbc5f3abbea06b9f
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-01-06 16:26:24 +0100

    git player

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,29 +5,86 @@ import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons f
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 import { IconContext } from "react-icons"; // for customazing the icons
 
-function Player() {
+export default function Player() {
     const [isPlaying, setIsPlaying] = useState(false);
+    const [time, setTime] = useState({
+      min: "",
+      sec: ""
+    });
+    const [currTime, setCurrTime] = useState({
+      min: "",
+      sec: ""
+    });
+  
+    const [seconds, setSeconds] = useState();
+  
     const [play, { pause, duration, sound }] = useSound(qala);
+  
+    useEffect(() => {
+      if (duration) {
+        const sec = duration / 1000;
+        const min = Math.floor(sec / 60);
+        const secRemain = Math.floor(sec % 60);
+        setTime({
+          min: min,
+          sec: secRemain
+        });
+      }
+    }, [isPlaying]);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        if (sound) {
+          setSeconds(sound.seek([]));
+          const min = Math.floor(sound.seek([]) / 60);
+          const sec = Math.floor(sound.seek([]) % 60);
+          setCurrTime({
+            min,
+            sec
+          });
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }, [sound]);
   
     const playingButton = () => {
       if (isPlaying) {
-        pause(); // this will pause the audio
+        pause();
         setIsPlaying(false);
       } else {
-        play(); // this will play the audio
+        play();
         setIsPlaying(true);
       }
     };
+  
     return (
       <div className="component">
         <h2>Playing Now</h2>
-        <img
-          className="musicCover"
-          src="https://picsum.photos/200/200"
-        />
+        <img className="musicCover" src="https://picsum.photos/200/200" />
         <div>
-          <h3 className="title">Lofi Study </h3>
-          <p className="subTitle">Pixabay - FASSounds</p>
+          <h3 className="title">Rubaiyyan</h3>
+          <p className="subTitle">Qala</p>
+        </div>
+        <div>
+          <div className="time">
+            <p>
+              {currTime.min}:{currTime.sec}
+            </p>
+            <p>
+              {time.min}:{time.sec}
+            </p>
+          </div>
+          <input
+            type="range"
+            min="0"
+            max={duration / 1000}
+            default="0"
+            value={seconds}
+            className="timeline"
+            onChange={(e) => {
+              sound.seek([e.target.value]);
+            }}
+          />
         </div>
         <div>
           <button className="playButton">
@@ -56,6 +113,4 @@ function Player() {
         </div>
       </div>
     );
-}
-
-export default Player;
+  }
